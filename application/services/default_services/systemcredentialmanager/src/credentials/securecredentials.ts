46272471c7ffe34b99efc8bd1039bd81d9fab527
gepinfo <info@geppettosoftware.com>
2023-03-22 12:17:48 +0000

    code commited from geppetto!

---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_accountingmonkstemplate_1158: 'mongodb://admin:password@',
+    DB_COLL_accountingmonkstemplate_1158: ':27017/accountingmonkstemplate_1158?authSource=admin'
+}
